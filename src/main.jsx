8fc76e148a3856b471ae1b8cdbceaaea3fd87e5a
taverNf <ntavernarofeliciano@gmail.com>
2025-08-20 11:08:12 -0300

    Correção do erro 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import TaskPage from './pages/TaskPage.jsx'
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
